bce94dc4961c285993a832dcb8627b6fd455bf0e
Peter <p.sivak91@gmail.com>
2022-12-20 12:47:08 -0800

    d

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
        <h1 className='text-center fs-3'>Week 5: Assignment 6: </h1>
        <br />
        <h2>Requirements: </h2>
-       <p>Continue work with your Next.js-powered full-stack application that reads data from at least two separate JSON files. Your app must use data fetching and pre-rendered static page generation to display a list of people by name on the home page. Use dynamic routing to create a page component to display complete details about individual persons that retrieves data from at least two JSON files to render links to other related persons. Use a Repl.it workspace to host your creation. Manage your application's code using a GitHub repository and git from your workspace shell's command-line interface.
+       <p>Continue work with your Next.js-powered full-stack application that reads data from at least two separate JSON files. Your app must use data fetching and pre-rendered static page generation to display a list of people by name on the home page. Use dynamic routing to create a page component to display complete details about individual persons that retrieves data from at least two JSON files to render links to other related persons. Use a Repl.it workspace to host your creation. Manage your application s code using a GitHub repository and git from your workspace shell s command-line interface.
 
-The final app should use the map() method within the code for the dynamically-routed page component [id].js file to display one or more related entries identified in your JSON-based data model.
+The final app should use the map() method within the code for the dynamically-routed page component id.js file to display one or more related entries identified in your JSON-based data model.
 
 Your development environment must also include functional installations of the Jest and React Testing libraries, with at least one working unit test documented. The working unit test must be different than the one demonstrated by the instructor.</p>
 
